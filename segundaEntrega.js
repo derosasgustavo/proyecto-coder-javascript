2e77e0191870860bb83793f9a95c1d1f3ea30a5b
derosasgustavo <derosasgustavo@gmail.com>
2022-11-12 01:56:13 -0300

    preparando entrega 3

--- segundaEntrega.js
+++ segundaEntrega.js
@@ -43,15 +43,28 @@ const mercaderia = [
   producto10,
   producto11,
 ];
-const carritoCompras = [];
-const precioCarrito = [];
-console.table(mercaderia);
 
-
+let buscadorProductosInput = document.getElementById(`buscador__productos`);
 let contenedorProductos = document.getElementById(`lista`);
 let carrito = document.getElementById(`carrito__compras`);
 let botones = document.getElementsByClassName(`boton`);
+let cuentaTotal = document.getElementById(`carrito__total`)
 
+let carritoComprasGuardado = [];
+
+//RENDERIZA EL CARRITO GUARDADO EN LOCAL STORAGE
+if (localStorage.getItem(`carrito`)) {
+  carritoComprasGuardado = JSON.parse(localStorage.getItem("carrito"));
+  console.log(carritoComprasGuardado)
+  for (const producto of carritoComprasGuardado) {
+    carrito.className = "carrito__contenedor";
+    carrito.innerHTML += `<div id="carrito__aparece${producto.id}" class="producto__carrito__aparece">
+   <p id="parrafo__carrito"> usted selecciono ${producto.nombre} </p>
+   <p id ="precio__producto"> precio: $${producto.precio}  </p>
+   </div>`;
+    
+  }
+ }
 
 function renderizarProductos() {
   //FUNCION QUE RENDERIZA TODOS LOS PRODUCTOS DE ARRAY MERCADERIA
@@ -61,399 +74,86 @@ function renderizarProductos() {
     productoListado.className = "producto";
     productoListado.innerHTML = `<h2> ${producto.nombre} </h2>
                                 <div class="imagen__tambor"><img src=" images/tambores.jpg " class="imagen__producto"  ></img></div>
-                                <p class="parrafo__producto"> breve descripcion del producto</p>
+                                
                                 <button class ="boton" id= "${producto.id}">agregar al carrito</button>`;
     contenedorProductos.appendChild(productoListado);
   }
 }
-
 renderizarProductos();
 
-
 function botonCarrito() {
-  // agrega los productos al carrito de compras
+  // agrega los productos al carrito de compras y hace push al array carritocomprasguradado
   for (const boton of botones) {
     boton.onclick = (e) => {
-      let productoBuscado = mercaderia.find(
+      const productoBuscado = mercaderia.find(
         (producto) => producto.id == e.target.id
       );
       carrito.className = "carrito__contenedor";
-      precioCarrito.push(productoBuscado.precio);
-      carrito.innerHTML += `<div id="carrito_aparece">
+      carrito.innerHTML += `<div id="carrito__aparece${productoBuscado.id}" class="producto__carrito__aparece">
    <p id="parrafo__carrito"> usted selecciono ${productoBuscado.nombre} </p>
    <p id ="precio__producto"> precio: $${productoBuscado.precio}  </p>
-         </div>`;
+   </div>`;
+      carritoComprasGuardado.push({
+        id: productoBuscado.id,
+        nombre: productoBuscado.nombre,
+        precio: productoBuscado.precio,
+      });
+    
+      localStorage.setItem(`carrito`, JSON.stringify(carritoComprasGuardado));
     };
   }
- 
-}
 
+}
 botonCarrito();
 
-let buscadorProductosInput = document.getElementById(`buscador__productos`);
-let botonBuscador = document.getElementById(`boton_buscador`);
-
-//necesito que el input pase su ingreso a uppercase,  donde se coloca?
+//renderizado del input
 
 buscadorProductosInput.oninput = () => {
- 
-  
   // renderiza los productos del imput
   let productoARenderizarBuscador = mercaderia.filter((producto) =>
     producto.nombre.includes(buscadorProductosInput.value)
   );
- 
-  if(productoARenderizarBuscador == ``){
-    let productoVacio  = document.getElementById(`lista`);
-   
-    productoVacio.innerHTML =`
-    <h2 class ="titulo__producto__inexistente"> LO LAMENTAMOS, NO TENEMOS ESE PRODUCTO </H2>`
-  }
-  else{
 
-  let productoListado = document.getElementById(`lista`);
-  productoListado.innerHTML = ``;
-  
-  for (const producto of productoARenderizarBuscador) {
-    let productoListado = document.createElement(`div`);
+  if (productoARenderizarBuscador == ``) {
+    let productoVacio = document.getElementById(`lista`);
+    productoVacio.innerHTML = `
+    <h2 class ="titulo__producto__inexistente"> LO LAMENTAMOS, NO TENEMOS ESE PRODUCTO </H2>`;
+  } else {
+    let productoListado = document.getElementById(`lista`);
+    productoListado.innerHTML = ``;
 
-    productoListado.className = "producto";
-    productoListado.innerHTML = `<h2> ${producto.nombre} </h2>
+    for (const producto of productoARenderizarBuscador) {
+      let productoListado = document.createElement(`div`);
+
+      productoListado.className = "producto";
+      productoListado.innerHTML = `<h2> ${producto.nombre} </h2>
                                 <img src=" images/tambores.jpg " class="imagen__producto"  ></img>
-                                <p class="parrafo__producto"> breve descripcion del producto</p>
+                               
                                 <button class ="boton" id= "${producto.id}">agregar al carrito</button>`;
-    contenedorProductos.appendChild(productoListado);
-
-    
-
-
+      contenedorProductos.appendChild(productoListado);
+    }
+    botonCarrito();
   }
-  botonCarrito();
-}}
-;
+};
 
 // BOTON QUE LIMPIA EL CARRITO
-  let botonLimpiarCarrito = document.getElementById(`limpiar__carrito`)
-  botonLimpiarCarrito.onclick = () => { carrito.innerHTML =``, carrito.className = ``, precioCarrito.splice(0)}
- 
+let colocarBoton = document.getElementsByClassName(`carrito__contenedor`);
+let botonLimpiarCarrito = document.getElementById(`limpiar__carrito`);
+botonLimpiarCarrito.onclick = () => {
+  (carrito.innerHTML = ``), (carrito.className = ``), (carritoComprasGuardado.splice(0)), (localStorage.clear(`carrito`));
+};
+
+
+
+
+
+    
   
   
-
-
-
-// function filtarPorPrecio() {   //funcion 1 filtar por precio FUNCIONA
-
-//   let eleecionPrecioFiltrado = parseInt(
-//     prompt(`filtrar por productos no mas caros de $ (se veran por consola)`)
-//   );
-//   let mercaderiaFiltrada = mercaderia.filter(
-//     (producto) => producto.precio < eleecionPrecioFiltrado
-//   );
-
-//   console.table(mercaderiaFiltrada);
-// }
-
-// function filtrarPorStock() {//funcion 2 filtar por stock FUNCIONA
-
-//   let eleecionStockFiltrado = parseInt(
-//     prompt(`filtrar por productos con un stock menor a (se ver por consola)`)
-//   );
-//   let mercaderiaFiltrada = mercaderia.filter(
-//     (producto) => producto.stock < eleecionStockFiltrado
-//   );
-
-//   console.table(mercaderiaFiltrada);
-// }
-// function quitarProducto() {     //EL USUARIO ELIGE PRODUCTO Y LO ELIMINA FUNCONA
-
-//   function elimina(array, numero) {
-//     array.splice(numero , 1);
-//   }
-
-//   alert(`identifique id del producto a eliminar`);
-//   let eleccionNumeroEliminar = parseInt(
-//     prompt(`numero de id del producto a eliminar`)
-//   );
-
-//   switch (eleccionNumeroEliminar) {
-//     case 0:
-//       elimina(mercaderia, eleccionNumeroEliminar);
-//       alert(`se eliminara el producto `);
-//       console.table(mercaderia);
-
-//       break;
-
-//     case 1:
-//       elimina(mercaderia, eleccionNumeroEliminar);
-
-//       alert(`se eliminara el producto `);
-//       console.table(mercaderia);
-
-//       break;
-
-//     case 2:
-//       elimina(mercaderia, eleccionNumeroEliminar);
-
-//       alert(`se eliminara el producto `);
-//       console.table(mercaderia);
-
-//       break;
-
-//     case 3:
-//       elimina(mercaderia, eleccionNumeroEliminar);
-//       alert(`se eliminara el producto `);
-//       console.table(mercaderia);
-
-//       break;
-
-//     case 4:
-//       elimina(mercaderia, eleccionNumeroEliminar);
-//       alert(`se eliminara el producto`);
-//       console.table(mercaderia);
-
-//       break;
-
-//     case 5:
-//       elimina(mercaderia, eleccionNumeroEliminar);
-//       alert(`se eliminara el producto`);
-//       console.table(mercaderia);
-
-//       break;
-
-//     case 6:
-//       elimina(mercaderia, eleccionNumeroEliminar);
-//       alert(`se eliminara el producto `);
-//       console.table(mercaderia);
-
-//       break;
-
-//     case 7:
-//       elimina(mercaderia, eleccionNumeroEliminar);
-//       alert(`se eliminara el producto `);
-//       console.table(mercaderia);
-
-//       break;
-
-//     case 8:
-//       elimina(mercaderia, eleccionNumeroEliminar);
-//       alert(`se eliminara el producto`);
-//       console.table(mercaderia);
-
-//       break;
-
-//     case 9:
-//       elimina(mercaderia, eleccionNumeroEliminar);
-//       alert(`se eliminara el producto`);
-//       console.table(mercaderia);
-
-//       break;
-
-//     default:
-//       break;
-//   }
-
-// }
-
-// function compraProducto(eleccionProducto) {
-//   // COMPRA MERCADERIA LO IMPRIME PANTALLA RESTA STOCK CREA ARRAY CARRITO Y ARRAY PRECIO CARRITO
-//   carritoCompras.push(mercaderia[eleccionProducto]);
-//   alert(`se agrego ${mercaderia[eleccionProducto].nombre} al carrito`);
-//   precioCarrito.push(mercaderia[eleccionProducto].precio);
-//   mercaderia[eleccionProducto].restaStock();
-//  }
-// function agregarCarrito() {
-//   //AGREGA PRODUCTO AL ARRAY CARRITO Y AL ARRAY PRECIO CARRITO
-
-//   let eleccionProducto = parseInt(
-//     prompt(`elija el producto (del 0 al 9)  escriba 99 para terminar`)
-//   );
-
-//   while (eleccionProducto !== 99) {
-//     switch (eleccionProducto) {
-//       case 0:
-//         compraProducto(eleccionProducto);
-
-//         break;
-
-//       case 1:
-//         compraProducto(eleccionProducto);
-//         break;
-
-//       case 2:
-//         compraProducto(eleccionProducto);
-//         break;
-
-//       case 3:
-//         compraProducto(eleccionProducto);
-//         break;
-
-//       case 4:
-//         compraProducto(eleccionProducto);
-//         break;
-
-//       case 5:
-//         compraProducto(eleccionProducto);
-//         break;
-
-//       case 6:
-//         compraProducto(eleccionProducto);
-//         break;
-
-//       case 7:
-//         compraProducto(eleccionProducto);
-//         break;
-
-//       case 8:
-//         compraProducto(eleccionProducto);
-//         break;
-
-//       case 9:
-//         compraProducto(eleccionProducto);
-//         break;
-
-//       default:
-//         alert(`producto inexistente`);
-//         break;
-//     }
-
-//     eleccionProducto = parseInt(
-//       prompt(`elija el producto (del 0 al 9)  escriba 99 para terminar`)
-//     );
-//   }
-// }
-
-// function ordenarPorPrecio() {//ordena por precio
-
-//   mercaderia.sort(function (a, b) {
-//     if (a.precio > b.precio) {
-//       return 1;
-//     }
-//     if (a.precio < b.precio) {
-//       return -1;
-//     }
-
-//     return 0;
-//   });
-//   console.table(mercaderia);
-// }
-// function ordenarPorNombre() { //ordena por nombre
-
-//   mercaderia.sort((function (a, b) {
-//     if (a.nombre > b.nombre) {
-//       return 1;
-//     }
-//     if (a.nombre < b.nombre) {
-//       return -1;
-//     }
-
-//     return 0;
-//   }))
-//   console.table(mercaderia)
-
-// }
-
-// function encontrar() { // busca un producto por su nombre y lo imprime en consola FUNCIONA
-
-//   let productoBuscado = prompt(`ingrese el nombre del producto a buscar`);
-//   const productoEncontrado = mercaderia.find(
-//     (el) => el.nombre === productoBuscado
-//   );
-//   console.table(productoEncontrado);
-// }
-// function filtradoConIncludes() { // filtrado con includes y agregado de MAP ==> devuelve los elementos del array del includes
-
-//   let eleccionUsuario = prompt(`busqueda por nombre`);
-//   const resultado = mercaderia.filter((prdo) =>
-//     prdo.nombre.includes(`${eleccionUsuario}`)
-//   );
-//   const listadoNombres = resultado.map(
-//     (el) => el.nombre + " hay en stock " + el.stock
-//   );
-//   console.table(listadoNombres);
-// }
-
-// function revisionCarrtio() { // funcion revision de carrito con reduce y map
-
-//   let revisionCarrito = prompt(
-//     `desea revisar su carrito de compras ingrese si o no`
-//   );
-//   if (revisionCarrito == "si") {
-//     const resultadoCarrito = carritoCompras.map(
-//       (el) => el.nombre + " " + " " + el.precio
-//     )
-//     let carritoFinal = document.getElementById(`listado__carrito`)
-//     carritoFinal.innerText = `usted ha incorporado los siguientes productos en su carrito ${resultadoCarrito}`
-
-//     console.log(`${resultadoCarrito}`);
-//     let revisionMontoCarrito = prompt(
-//       `desea revisar el monto total de su carrito de compras ingrese si o no`
-//     );
-//     if (revisionMontoCarrito == "si") {
-//       const montoTotalCarrito = precioCarrito.reduce(
-//         (acumulador, elemento) => acumulador + elemento,
-//         0
-//       )
-//       let carritoFinal = document.getElementById(`listado__carrito__dinero`)
-//     carritoFinal.innerText = `usted lleva gastados $ ${montoTotalCarrito} en su compra`;
-//       console.log(`usted lleva gastados $ ${montoTotalCarrito} en su compra`);
-//     }
-//   } else {
-//     alert(`programa terminado`);
-//   }
-// }
-
-// let alertInicio =console.log(
-//   `tenemos los siguientes productos \n se veran en la consola!! y ahora tambien en el index.html jajajaja`
-// );
-// for (const producto of mercaderia) {
-//   alertInicio += `\n ${producto.id} --- ${producto.nombre} a $ ${producto.precio}`;
-//   console.log(alertInicio);
-// }
-
-//  let eleccionInicial = parseInt(prompt (`MENU algunas acciones se muestran como log y otras como table en consola \n 1-- comprar \n 2--filtrar por precio \n 3-- filtrar por stock \n 4--quitar producto \n 5 ordenar alfabeticamente \n 6-- ordenar por precio \n 7-- filtrar nombre y map \n 8-- terminar`))
-
-//  while (eleccionInicial !== 8){
-// switch (eleccionInicial) {
-//   case 1: agregarCarrito()
-//   eleccionInicial = parseInt(prompt (`MENU \n 1-- comprar \n 2--filtrar por precio \n 3-- filtrar por stock \n 4--quitar producto \n 5 ordenar alfabeticamente \n 6-- ordenar por precio \n 7-- filtrar nombre y map \n 8-- terminar`))
-
-//     break;
-
-//     case 2: filtarPorPrecio()
-//     eleccionInicial = parseInt(prompt (`MENU \n 1-- comprar \n 2--filtrar por precio \n 3-- filtrar por stock \n 4--quitar producto \n 5 ordenar alfabeticamente \n 6-- ordenar por precio \n 7-- filtrar nombre y map \n 8-- terminar`))
-
-//     break;
-
-//     case 3: filtrarPorStock()
-//     eleccionInicial = parseInt(prompt (`MENU \n 1-- comprar \n 2--filtrar por precio \n 3-- filtrar por stock \n 4--quitar producto \n 5 ordenar alfabeticamente \n 6-- ordenar por precio \n 7-- filtrar nombre y map \n 8-- terminar`))
-
-//     break;
-
-//     case 4: quitarProducto()
-//     eleccionInicial = parseInt(prompt (`MENU \n 1-- comprar \n 2--filtrar por precio \n 3-- filtrar por stock \n 4--quitar producto \n 5 ordenar alfabeticamente \n 6-- ordenar por precio \n 7-- filtrar nombre y map \n 8-- terminar`))
-
-//     break;
-
-//     case 5: ordenarPorNombre()
-//     eleccionInicial = parseInt(prompt (`MENU \n 1-- comprar \n 2--filtrar por precio \n 3-- filtrar por stock \n 4--quitar producto \n 5 ordenar alfabeticamente \n 6-- ordenar por precio \n 7-- filtrar nombre y map \n 8-- terminar`))
-
-//     break;
-
-//     case 6: ordenarPorPrecio()
-//     eleccionInicial = parseInt(prompt (`MENU \n 1-- comprar \n 2--filtrar por precio \n 3-- filtrar por stock \n 4--quitar producto \n 5 ordenar alfabeticamente \n 6-- ordenar por precio \n 7-- filtrar nombre y map \n 8-- terminar`))
-
-//     break;
-
-//     case 7: filtradoConIncludes()
-//     eleccionInicial = parseInt(prompt (`MENU \n 1-- comprar \n 2--filtrar por precio \n 3-- filtrar por stock \n 4--quitar producto \n 5 ordenar alfabeticamente \n 6-- ordenar por precio \n 7-- filtrar nombre y map \n 8-- terminar`))
-
-//     break;
-
-//   default: eleccionInicial = parseInt(prompt (`MENU \n 1-- comprar \n 2--filtrar por precio \n 3-- filtrar por stock \n 4--quitar producto \n 5 ordenar alfabeticamente \n 6-- ordenar por precio \n 7-- filtrar nombre y map \n 8-- terminar`))
-
-//     break;
-
-//   }
-// }
-
-// revisionCarrtio()
+// let botonMontoCarrito = document.getElementById(`boton__seccion__thinner`)
+// botonMontoCarrito.onclick = () => {
+// console.log(carritoComprasGuardado)
+// carritoMonto = carritoComprasGuardado.map ((el) => el.precio)
+// console.log(carritoMonto)
+// let sumaCarrito = carritoMonto.reduce((acc,numero) => acc + numero);
+// console.log(sumaCarrito);
